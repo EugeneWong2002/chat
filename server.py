30d04e4020cb2b75aa1123f951f86dc074c6a0a7
EugeneWong2002 <wongweemin2002@outlook.com>
2021-01-11 16:05:22 +0800

    Server accepts user config for port and ip

--- server.py
+++ server.py
@@ -1,15 +1,25 @@
 import socket
 
 
-
 sock = socket.socket()
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-sock.bind(("0.0.0.0", 7654))
+ip = input("Enter ip: ")
+port = input("Enter port: ")
+
+sock.bind((ip, int(port)))
 
 sock.listen(12)
 
+
+
+
+
 cli_sock, cli_add = sock.accept()
-print("connection from" + str(cli_add[1]))
-message = cli_sock.recv(12)
-print(message.decode())
+print("Connection from " + str(cli_add[1]))
+try:
+    message = cli_sock.recv(2048)
+except Exception as e:
+    print("Something went wrong")
+else:
+    print(message.decode())
